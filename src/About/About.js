2e602b4d36f00c777784a38a8699f7b7b260ccd8
mertbagt <73559781+mertbagt@users.noreply.github.com>
2021-02-08 17:07:08 -0500

    tweaked about page

--- src/About/About.js
+++ src/About/About.js
@@ -7,24 +7,26 @@ class About extends Component {
     return (
       <>
         <h2>About</h2>
+        <div className="about">
         <p>This is a basic app for dinner planning</p>
         <br/>
-        <p>At the top, click on:</p>
+        <h3>At the top, click on:</h3>
         <p>&nbsp;&nbsp;&nbsp;'menu' to see your weekly menu</p>
         <p>&nbsp;&nbsp;&nbsp;'reset' to wipe your menu and start over</p>
         <p>&nbsp;&nbsp;&nbsp;'about' to return to the instructions page</p>
         <br/>
-        <p>On the Menu Screen</p>
-        <p>When adding a new dish to your menu:</p>
+        <h3>On the Menu Screen</h3>
+        <h4>When adding a new dish to your menu:</h4>
         <p>&nbsp;&nbsp;&nbsp;select a day, a category and type in the name of the dish</p>
         <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;or</p>
         <p>&nbsp;&nbsp;&nbsp;select a day, a category and press the random button</p>
         <br/>
-        <p>Click on the '-' button to remove a dish from its respective day</p>
+        <p>&nbsp;&nbsp;&nbsp;Click on the '-' button to remove a dish from its respective day</p>
         <br/>
-        <p>On smaller display widths:</p>
-        <p>Click on the name of a day to shrink or expand it</p>
+        <h4>On smaller display widths:</h4>
+        <p>&nbsp;&nbsp;&nbsp;Click on the name of a day to shrink or expand it</p>
         <br/>
+        </div>
       </>
     );
   }  
